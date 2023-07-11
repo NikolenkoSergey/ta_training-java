940db13951def54261a9ebc8efc8f658634b4c89
Nikolenko Sergey <nikolenko.s.y@gmail.com>
2023-07-11 18:49:42 +0600

    work test

--- src/main/java/com/epam/training/student_sergey_nikolenko/Main.java
+++ src/main/java/com/epam/training/student_sergey_nikolenko/Main.java
@@ -3,5 +3,16 @@ package com.epam.training.student_sergey_nikolenko;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(evenNumber(-1));
+
+
+    }
+
+    public static boolean evenNumber(int number){
+        if (number % 2 == 0){
+            return true;
+        }
+        return false;
     }
 }
+
---
+++ src/test/java/com/epam/training/student_sergey_nikolenko/MainTest.java
@@ -0,0 +1,12 @@
+package com.epam.training.student_sergey_nikolenko;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class MainTest extends TestCase {
+
+    @Test
+    public void testEvenNumber() {
+        assertEquals(true, true);
+    }
+}
