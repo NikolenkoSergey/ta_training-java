d245dfb6b58b0d27ffd07dade884a358dbf46658
Nikolenko Sergey <nikolenko.s.y@gmail.com>
2023-07-12 20:57:26 +0600

    HelloAutocode

---
+++ src/main/java/com/epam/training/student_sergey_nikolenko/HelloAutocode.java
@@ -0,0 +1,9 @@
+package com.epam.training.student_sergey_nikolenko;
+
+public class HelloAutocode {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Autocode!");
+    }
+
+}
--- src/main/java/com/epam/training/student_sergey_nikolenko/Main.java
+++ src/main/java/com/epam/training/student_sergey_nikolenko/Main.java
@@ -2,8 +2,8 @@ package com.epam.training.student_sergey_nikolenko;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println(evenNumber(-1));
+       System.out.println("Hello world!");
+//        System.out.println(evenNumber(-1));
 
 
     }
